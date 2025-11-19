e40f4ead3fc2032e49ee05fa778c92953133fff7
Yao <yaochengjames@gmail.com>
2025-11-19 11:17:20 -0800

    update main

--- dynamic_mpu/src/main.c
+++ dynamic_mpu/src/main.c
@@ -3,10 +3,7 @@
 #include "uart.h"
 #include "benchmark.h"
 #include "test_tasks.h"
-
-#ifdef CONFIG_STATIC
-#include "mpu_static.h"
-#endif
+#include "mpu_dynamic.h"
 
 int main(void)
 {
@@ -15,25 +12,16 @@ int main(void)
     
     uart_print("\n");
     uart_print("=====================================\n");
-
-#ifdef CONFIG_BASELINE
-    uart_print("  Baseline Configuration (No MPU)   \n");
-#elif defined(CONFIG_STATIC)
-    uart_print("  Static MPU Configuration          \n");
-#endif
-
+    uart_print("  Dynamic MPU Configuration         \n");
     uart_print("=====================================\n");
     uart_print("CPU: ARM Cortex-M4 @ 50MHz\n");
     uart_print("RTOS: FreeRTOS\n");
     uart_print("Platform: QEMU MPS2-AN386\n");
     uart_print("=====================================\n\n");
 
-#ifdef CONFIG_STATIC
-    mpu_static_init();
-    mpu_static_enable();
-    mpu_static_print_config();
+    mpu_dynamic_init();
+    mpu_dynamic_print_config();
     uart_print("\n");
-#endif
 
     test_create_tasks();
     
