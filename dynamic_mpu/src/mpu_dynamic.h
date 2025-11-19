459df27d8829a6fdb779d916c91b505ae049c0d3
Yao <yaochengjames@gmail.com>
2025-11-18 22:28:53 -0800

    update dynamic header

--- dynamic_mpu/src/mpu_dynamic.h
+++ dynamic_mpu/src/mpu_dynamic.h
@@ -1,3 +1,20 @@
 #ifndef MPU_DYNAMIC_H
 #define MPU_DYNAMIC_H
 
+#include <stdint.h>
+#include "FreeRTOS.h"
+#include "task.h"
+
+typedef struct {
+    uint32_t rbar[8];
+    uint32_t rasr[8];
+    uint8_t num_regions;
+    const char* task_name;
+} mpu_task_config_t;
+
+void mpu_dynamic_init(void);
+void mpu_dynamic_switch_task(void *task_handle);
+uint32_t mpu_dynamic_get_switches(void);
+void mpu_dynamic_print_config(void);
+
+#endif
